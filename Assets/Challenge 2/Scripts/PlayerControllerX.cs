3ce565accd67c6685d80d46b3b67a3deaab360a8
darkhorse <managoy52@gmail.com>
2020-12-21 13:01:19 +0545

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float lastTime;
+
+    private void Start()
+    {
+        lastTime = Time.time;   
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && (Time.time - lastTime > 1.0f))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lastTime = Time.time;           
+        }
+     
+    }
+}
---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 30.0f;
+    private float lowerBound = -10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If an object goes past the players view in the game, remove the object
+        if(transform.position.z > topBound)
+        {
+            Destroy(this.gameObject);
+        }else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over!!");
+            Destroy(this.gameObject);
+        }
+    }
+}
